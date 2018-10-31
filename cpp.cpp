a332256712d4f6e93a3ac957526e375dee176ead
Smailil <33234842+Smailil@users.noreply.github.com>
2018-10-31 18:54:14 +0300

    Update cpp.cpp

--- cpp.cpp
+++ cpp.cpp
@@ -3,12 +3,13 @@ using namespace std;
 int main()
 {
 	int x,b,p=0,k=1,la=-1;
+	cout << "Max Ctepen: "
 	cin >> x;
 	int atat[x];
 	x++;
 	int arr[x];
 	for (int i=0; i<x; i++)
-	{
+	{	
 		cin >> b;
 		arr[i]=b;
 	}
